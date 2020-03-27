0d39805a85282f21c1230a2dbf4cf2e96098eae4
LaBatata101 <labatata101@linuxmail.org>
2020-03-26 21:13:19 -0300

    implement RentDAO

--- Habitarium-Gitflow-Maven/src/main/java/dao/RentDAO.java
+++ Habitarium-Gitflow-Maven/src/main/java/dao/RentDAO.java
@@ -1,4 +1,47 @@
 package main.java.dao;
 
-public class RentDAO {
+import main.java.connection.ConnectionFactory;
+import main.java.entity.Rent;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
+
+public class RentDAO implements DAO<Rent> {
+
+   private EntityManager entityManager = new ConnectionFactory().getConnection();
+
+   @Override
+   public void save(Rent object) {
+      try {
+         this.entityManager.getTransaction().begin();
+         this.entityManager.persist(object);
+         this.entityManager.getTransaction().commit();
+      } catch (Exception error) {
+         this.entityManager.getTransaction().rollback();
+      } finally {
+         this.entityManager.close();
+      }
+   }
+
+   @Override
+   public List get() {
+      Query query = this.entityManager.createQuery("SELECT r FROM Rent as r");
+      return query.getResultList();
+   }
+
+   @Override
+   public Rent update(Rent object) {
+      Rent rent = null;
+      try {
+         this.entityManager.getTransaction().begin();
+         rent = this.entityManager.merge(object);
+         this.entityManager.getTransaction().commit();
+      } catch (Exception exception) {
+         this.entityManager.getTransaction().rollback();
+      } finally {
+         this.entityManager.close();
+      }
+      return rent;
+   }
 }
