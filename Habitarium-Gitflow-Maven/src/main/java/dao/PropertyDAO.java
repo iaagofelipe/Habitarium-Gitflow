cb4e9385f997d74b818041e94f26741125805356
LaBatata101 <labatata101@linuxmail.org>
2020-03-26 21:12:52 -0300

    implement PropertyDAO

--- Habitarium-Gitflow-Maven/src/main/java/dao/PropertyDAO.java
+++ Habitarium-Gitflow-Maven/src/main/java/dao/PropertyDAO.java
@@ -1,4 +1,47 @@
 package main.java.dao;
 
-public class PropertyDAO {
+import main.java.connection.ConnectionFactory;
+import main.java.entity.Property;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
+
+public class PropertyDAO implements DAO<Property> {
+
+    private EntityManager entityManager = new ConnectionFactory().getConnection();
+
+    @Override
+    public void save(Property object) {
+        try {
+            this.entityManager.getTransaction().begin();
+            this.entityManager.persist(object);
+            this.entityManager.getTransaction().commit();
+        } catch (Exception error) {
+            this.entityManager.getTransaction().rollback();
+        } finally {
+            this.entityManager.close();
+        }
+    }
+
+    @Override
+    public List get() {
+        Query query = this.entityManager.createQuery("SELECT p FROM Property as p");
+        return query.getResultList();
+    }
+
+    @Override
+    public Property update(Property object) {
+        Property property = null;
+        try {
+            this.entityManager.getTransaction().begin();
+            property = this.entityManager.merge(object);
+            this.entityManager.getTransaction().commit();
+        } catch (Exception exception) {
+            this.entityManager.getTransaction().rollback();
+        } finally {
+            this.entityManager.close();
+        }
+        return property;
+    }
 }
