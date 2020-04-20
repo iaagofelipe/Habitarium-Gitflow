51c956a1510b3ba9a61507d8892bcf1c3d149991
LaBatata101 <labatata101@linuxmail.org>
2020-04-20 20:09:29 -0300

    add method to start rent

--- Habitarium-Gitflow-Maven/src/main/java/controller/RentController.java
+++ Habitarium-Gitflow-Maven/src/main/java/controller/RentController.java
@@ -1,13 +1,27 @@
 package main.java.controller;
 
-import main.java.dao.RentDAO;
 import main.java.dataUtils.DataUtil;
 import main.java.entity.Rent;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.*;
 
 public class RentController {
+
+    public static void startRent(Rent rent) {
+        // @TODO: LOOP infinito resolver
+        Date entraceDate = rent.getEntranceDate();
+        while (entraceDate.before(rent.getExitDate())) {
+            int payDay = rent.getPayDay();
+            int month = DataUtil.getMonth(entraceDate);
+            int year = DataUtil.getYear(entraceDate);
+
+            GregorianCalendar date = new GregorianCalendar(year, month - 1, payDay);
+            System.out.println(date.getTime());
+            rent.setMonthToBePaid(date.getTime(), false);
+        }
+    }
 
     public static void makePayment(Rent rent, Date date, Float value) {
         rent.setDatePaidAndValue(date, value);
@@ -25,10 +39,22 @@ public class RentController {
         Map<Date, Boolean> toBePaid = rent.getMonthToBePaid();
 
         for (Date date : toBePaid.keySet()) {
-            boolean paid = toBePaid.get(date);
-            if (!paid)
+            boolean value = toBePaid.get(date);
+            if (!value)
                 notPaid.add(date);
         }
         return notPaid;
+    }
+
+    public static List<Date> monthsPaid(Rent rent) {
+        List<Date> paid = new ArrayList<>();
+        Map<Date, Boolean> toBePaid = rent.getMonthToBePaid();
+
+        for (Date date : toBePaid.keySet()) {
+            boolean value = toBePaid.get(date);
+            if (value)
+                paid.add(date);
+        }
+        return paid;
     }
 }
