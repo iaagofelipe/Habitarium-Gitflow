58fbf05abc10334db0d9ec10653287e9b3fb85b7
LaBatata101 <labatata101@linuxmail.org>
2020-07-25 16:47:09 -0300

    make Habitarium-Backend a java module

--- Habitarium/src/main/java/module-info.java
+++ Habitarium/src/main/java/module-info.java
@@ -2,6 +2,13 @@ module com.habitarium.back {
     requires java.sql;
     requires net.bytebuddy;
     requires java.persistence;
+    requires com.fasterxml.classmate;
+    requires java.xml.bind;
 
-    exports com.habitarium;
+
+    opens com.habitarium.entity;
+    exports com.habitarium.controller;
+    exports com.habitarium.entity;
+    exports com.habitarium.dao;
+    exports com.habitarium.enuns;
 }
